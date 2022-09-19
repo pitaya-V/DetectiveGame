2eac19dff7bc0e4c6ec19a75ee444d88cd079a04
Hzw <unityhzw@gmail.com>
2022-09-19 09:30:39 +0800

    【初始化】初始化项目工程，引入GF和基础的一些配置

---
+++ DetectiveGame/Assets/GameMain/Scripts/Base/GameEntry.Custom.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityGameFramework.Runtime;
+
+namespace DetectiveGame
+{
+    /// <summary>
+    /// 游戏入口。
+    /// </summary>
+    public partial class GameEntry : MonoBehaviour
+    {
+
+
+        private static void InitCustomComponents()
+        {
+
+        }
+    }
+}

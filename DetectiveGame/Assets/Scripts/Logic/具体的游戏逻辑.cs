41deeb84b0eb09c43d93328d7e2a1445bfe412d1
Hzw <unityhzw@gmail.com>
2022-10-19 16:00:09 +0800

    调整一次目录结构

---
+++ DetectiveGame/Assets/Scripts/Logic/具体的游戏逻辑.cs
@@ -0,0 +1,21 @@
+﻿#region HeadComments
+// ********************************************************************
+//  Copyright (C) 2022 DefaultCompany
+//  作    者：unity
+//  文件路径：Assets/GameMain/Logic/具体的游戏逻辑.cs
+//  创建日期：2022/10/19 15:53:47
+//  功能描述：
+//
+// *********************************************************************
+#endregion
+
+using UnityEngine;
+using System.Collections;
+namespace DetectiveGame
+{
+	public class 具体的游戏逻辑 : MonoBehaviour 
+	{
+
+	}
+}
+
---
+++ DetectiveGame/Assets/[Resources]/_Art/美术原始资源.cs
@@ -0,0 +1,21 @@
+﻿#region HeadComments
+// ********************************************************************
+//  Copyright (C) 2022 DefaultCompany
+//  作    者：unity
+//  文件路径：Assets/[Resources]/_Art/美术原始资源.cs
+//  创建日期：2022/10/19 15:56:05
+//  功能描述：
+//
+// *********************************************************************
+#endregion
+
+using UnityEngine;
+using System.Collections;
+namespace DetectiveGame
+{
+	public class 美术原始资源 : MonoBehaviour 
+	{
+
+	}
+}
+

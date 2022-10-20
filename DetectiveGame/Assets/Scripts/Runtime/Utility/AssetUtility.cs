165135a20cfaca8b133eaaa440c5d5fd6836425d
Hzw <unityhzw@gmail.com>
2022-10-20 10:47:31 +0800

    修改资源加载路径

--- DetectiveGame/Assets/Scripts/Runtime/Utility/AssetUtility.cs
+++ DetectiveGame/Assets/Scripts/Runtime/Utility/AssetUtility.cs
@@ -13,52 +13,52 @@ namespace DetectiveGame
     {
         public static string GetConfigAsset(string assetName, bool fromBytes)
         {
-            return Utility.Text.Format("[Resources]/Assets/GameMain/Configs/{0}.{1}", assetName, fromBytes ? "bytes" : "txt");
+            return Utility.Text.Format("Assets/[Resources]/Configs/{0}.{1}", assetName, fromBytes ? "bytes" : "txt");
         }
 
         public static string GetDataTableAsset(string assetName, bool fromBytes)
         {
-            return Utility.Text.Format("[Resources]/Assets/GameMain/DataTables/{0}.{1}", assetName, fromBytes ? "bytes" : "txt");
+            return Utility.Text.Format("Assets/[Resources]/DataTables/{0}.{1}", assetName, fromBytes ? "bytes" : "txt");
         }
 
         public static string GetDictionaryAsset(string assetName, bool fromBytes)
         {
-            return Utility.Text.Format("[Resources]/Assets/GameMain/Localization/{0}/Dictionaries/{1}.{2}", GameEntry.Localization.Language, assetName, fromBytes ? "bytes" : "xml");
+            return Utility.Text.Format("Assets/[Resources]/Localization/{0}/Dictionaries/{1}.{2}", GameEntry.Localization.Language, assetName, fromBytes ? "bytes" : "xml");
         }
 
         public static string GetFontAsset(string assetName)
         {
-            return Utility.Text.Format("[Resources]/Assets/GameMain/Fonts/{0}.ttf", assetName);
+            return Utility.Text.Format("Assets/[Resources]/Fonts/{0}.ttf", assetName);
         }
 
         public static string GetSceneAsset(string assetName)
         {
-            return Utility.Text.Format("[Resources]/Assets/GameMain/Scenes/{0}.unity", assetName);
+            return Utility.Text.Format("Assets/[Resources]/Scenes/{0}.unity", assetName);
         }
 
         public static string GetMusicAsset(string assetName)
         {
-            return Utility.Text.Format("[Resources]/Assets/GameMain/Music/{0}.mp3", assetName);
+            return Utility.Text.Format("Assets/[Resources]/Music/{0}.mp3", assetName);
         }
 
         public static string GetSoundAsset(string assetName)
         {
-            return Utility.Text.Format("[Resources]/Assets/GameMain/Sounds/{0}.wav", assetName);
+            return Utility.Text.Format("Assets/[Resources]/Sounds/{0}.wav", assetName);
         }
 
         public static string GetEntityAsset(string assetName)
         {
-            return Utility.Text.Format("[Resources]/Assets/GameMain/Entities/{0}.prefab", assetName);
+            return Utility.Text.Format("Assets/[Resources]/Entities/{0}.prefab", assetName);
         }
 
         public static string GetUIFormAsset(string assetName)
         {
-            return Utility.Text.Format("[Resources]/Assets/GameMain/UI/UIForms/{0}.prefab", assetName);
+            return Utility.Text.Format("Assets/[Resources]/UI/UIForms/{0}.prefab", assetName);
         }
 
         public static string GetUISoundAsset(string assetName)
         {
-            return Utility.Text.Format("[Resources]/Assets/GameMain/UI/UISounds/{0}.wav", assetName);
+            return Utility.Text.Format("Assets/[Resources]/UI/UISounds/{0}.wav", assetName);
         }
     }
 }
